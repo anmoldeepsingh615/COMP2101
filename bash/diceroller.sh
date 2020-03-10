f6cdf0c378727e8ab3f6855bd7e8dca48ec5f680
anmoldeepsingh615 <anmoldeepsingh615>
2020-03-09 17:18:59 -0700

    after mid

--- bash/diceroller.sh
+++ bash/diceroller.sh
@@ -6,11 +6,11 @@
 # Task: Improve this script by making it roll as many dice as there are letters in your first name, instead of just one
 
 # roll the dice and display the result
-echo "
-Rolling...
-$(( RANDOM % 6 + 1)) rolled
-"
-echo "
-Rolling
-$((  RANDOM % 6 + 1 )), ((  RANDOM % 6 + 1 )), ((  RANDOM % 6 + 1 )), ((  RANDOM % 6 + 1 )), ((  RANDOM % 6 + 1 ))
-"
+read -p "Enter your name: " name
+charl=$(echo "$name" | wc -m)
+echo "Rolling....."
+for (( i=1; i<${charl}+1; i++ ));
+do
+a=$(( RANDOM % 6 + 1))
+echo $a
+done
